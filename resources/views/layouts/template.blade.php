b8b068ad16ae100bd16f4a30ea759e74a43b1c91
Kilak <info@kilak.com>
2023-06-14 16:56:09 -0300

    Actualización de layout

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -1,19 +1,22 @@
 <!doctype html>
-<html lang="es">
+<html class="h-100" lang="es">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Kilak Facturador</title>
     <link rel="shortcut icon" href="{{asset('img/isotipo.png')}}">
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sticky-footer-navbar/">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}"">
   </head>
-  <body>
+  <body class="d-flex flex-column h-100">
   
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
       <div class="container">
-        <a class="navbar-brand" href="#">Kilak</a>
+        <a class="navbar-brand" href="#">
+          <img src="{{asset('img/kilak-logo.png')}}" alt="Kilak Design Studio">
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -42,13 +45,15 @@
       </div>
     </nav>
 
-    <div class="container py-5">
-      <h1>@yield('h1')</h1>
-      @yield('content')
-    </div>
-
-    <footer class="bg-secondary p-3">
+    <main class="flex-shrink-0">
       <div class="container">
+        <h1>@yield('h1')</h1>
+        @yield('content')
+      </div>
+    </main>
+
+    <footer class="footer mt-auto p-3 bg-dark">
+      <div class="container text-light text-end">
         Copyright Kilak 2023 <i class="bi bi-globe"></i> <a href="https://kilak.com">kilak.com</a>
       </div>
     </footer>
